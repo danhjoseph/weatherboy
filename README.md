3d955efe86b5d469bcf761b7836bba64df3749aa
Dan Joseph <54128367+danhjoseph@users.noreply.github.com>
2022-06-12 11:28:08 -0400

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # weatherboy
- 
+
+I'm Twitter bot that tweets back the current temperature and conditions.
+
+## Demo
+
+<img src="Demo/weatherboy_demo.gif" width="400" height="575"/>
+
+## Built With
+
+- [Python](https://github.com/python)
+- [Google Cloud Platform](https://github.com/GoogleCloudPlatform)
+
+## Ackowledgments
+
+- [tweepy](https://github.com/tweepy/)
+- [Weather API](https://github.com/weatherapicom/)
--- weatherboy.py
+++ weatherboy.py
@@ -11,6 +11,7 @@ def main():
     consumer_secret = os.getenv('CONSUMER_SECRET')
     access_token = os.getenv('ACCESS_TOKEN')
     access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+    api_key = os.getenv('API_KEY')
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -19,11 +20,11 @@ def main():
 
     twit_handle = "@heyweatherboy"
     query = api.mentions_timeline()
-    result = query[0].text.strip("@heyweatherboy ")
+    result = query[0].text.replace("@heyweatherboy ", "")
     number_of_tweets = 1
 
     response = requests.get(
-        "https://api.weatherapi.com/v1/current.json?key=032e7d2533b0484e8ac175455221106&q="+result+"&aqi=no").json()
+        "https://api.weatherapi.com/v1/current.json?key="+api_key+"&q="+result+"&aqi=no").json()
 
     temp = response["current"]["temp_f"]
     condition = response["current"]["condition"]["text"]
